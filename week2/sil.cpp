340a713168d64cf1112ee3d716a02998167ad545
Jakub Krukowski <62674442+KrukowskiJ@users.noreply.github.com>
2020-04-05 00:01:47 +0200

    Update sil.cpp
    delete using namespace std;

--- week2/sil.cpp
+++ week2/sil.cpp
@@ -1,15 +1,12 @@
 #include <bits/stdc++.h>                                                        
-                                                                                
-using namespace std;                                                            
-
-                                                                                
+                                                                                                                                                         
 long long  n,a=1;                                                               
                                                                                 
 int main(){                                                                     
-    scanf("%lld",&n);                                                           
+    scanf("%lld",&n);  
     for(long long i=2;i<=n;i++)                                                 
         a*=i;                                                                   
                                                                                 
-    printf("%d",a%10);                                                                 
+  printf("%d",a%10);                                                                 
   return 0;                                                                   
 } 
